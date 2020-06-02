9534358370984ff5243d01789ced803e35833f4a
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-03 00:09:10 +0200

    pduck

---
+++ cmds/pduckiduck.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const colours = require("../colours.json");
+
+module.exports.run = async (bot, message, args) => {
+  let aEmbed = new Discord.MessageEmbed()
+    .setColor(colours.green_light)
+    .setTitle("Profile GLR")
+    .setThumbnail(message.guild.iconURL())
+    .addField(
+      "Pour aller sur le profil de **DuckiDuck**",
+      "[Clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717465384281702520)"
+    )
+    .setFooter(
+      `No Limit - Profil GLR `,
+      bot.user.displayAvatarURL("png", true)
+    );
+  message.channel.send(aEmbed);
+};
+
+module.exports.help = {
+  name: `pduck`,
+};
