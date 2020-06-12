0f89cc8ef6be5943aa1a8bd3063ecf9a6a228e94
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-13 00:30:21 +0200

    update padrion3

--- cmds/padrion.js
+++ cmds/padrion.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Profile GLR")
     .setThumbnail(message.guild.iconURL())
     .addField(
-      "Le profil IG de",
-      "<@301013425830625282> est disponible [ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717464104557740073)"
+      "Pour aller sur le profil de",
+      "<@301013425830625282> [clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717464104557740073)"
     )
     .setFooter(
       `No Limit - Profil GLR `,
