c95da9250bea51161385f1e43198c57d86b24162
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-14 00:02:57 +0200

    Update commande pwal

--- cmds/pwal.js
+++ cmds/pwal.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Profile GLR")
     .setThumbnail(message.guild.iconURL())
     .addField(
-      "Pour aller sur le profil de **WคɭกคՇร**",
-      "[Clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717467005711548483)"
+      "Pour aller sur le profil de",
+      "<@485028821662433280> [clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717467005711548483)"
     )
     .setFooter(
       `No Limit - Profil GLR `,
