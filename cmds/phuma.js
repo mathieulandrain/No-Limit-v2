014001f90cbdc76799e8958cc993e264f9050acf
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-13 23:26:42 +0200

    Update commande phuma

--- cmds/phuma.js
+++ cmds/phuma.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Profile GLR")
     .setThumbnail(message.guild.iconURL())
     .addField(
-      "Pour aller sur le profil de **Humanity**",
-      "[Clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717465751836950639)"
+      "Pour aller sur le profil de",
+      "<@612050721885388801> [clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717465751836950639)"
     )
     .setFooter(
       `No Limit - Profil GLR `,
