70fc737d1447e134989eb91df6654b0ed8523f2a
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-13 23:23:18 +0200

    Update commande pducki

--- cmds/pduckiduck.js
+++ cmds/pduckiduck.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Profile GLR")
     .setThumbnail(message.guild.iconURL())
     .addField(
-      "Pour aller sur le profil de **DuckiDuck**",
-      "[Clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717465384281702520)"
+      "Pour aller sur le profil de",
+      "<@380768037089050625> [clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717465384281702520)"
     )
     .setFooter(
       `No Limit - Profil GLR `,
