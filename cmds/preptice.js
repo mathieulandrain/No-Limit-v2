3e0b87dc26cd104e0b9ba4e2bde9ef88d3f9c16e
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-13 23:57:08 +0200

    Update commande preptice

--- cmds/preptice.js
+++ cmds/preptice.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Profile GLR")
     .setThumbnail(message.guild.iconURL())
     .addField(
-      "Pour aller sur le profil de **Reptice**",
-      "[Clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717466664677146636)"
+      "Pour aller sur le profil de",
+      "<@249963632912367626> [clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717466664677146636)"
     )
     .setFooter(
       `No Limit - Profil GLR `,
