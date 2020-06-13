6623ebbfea6e48a50b4f295d9516ef6f978c5054
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-13 23:55:34 +0200

    Update commande pprodu

--- cmds/pprodu006.js
+++ cmds/pprodu006.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Profile GLR")
     .setThumbnail(message.guild.iconURL())
     .addField(
-      "Pour aller sur le profil de **produ006**",
-      "[Clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717466335160041532)"
+      "Pour aller sur le profil de",
+      "<@214631925191540736> [clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717466335160041532)"
     )
     .setFooter(
       `No Limit - Profil GLR `,
