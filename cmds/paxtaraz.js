11bfffffc70c98c6305fa6f0770e69589bd9170c
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-13 23:17:04 +0200

    Update commande pax

--- cmds/paxtaraz.js
+++ cmds/paxtaraz.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Profile GLR")
     .setThumbnail(message.guild.iconURL())
     .addField(
-      "Pour aller sur le profil de **Axstarz**",
-      "[Clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717465069386072085)"
+      "Pour aller sur le profil de",
+      "<@259286602927308800> [clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717465069386072085)"
     )
     .setFooter(
       `No Limit - Profil GLR `,
