18802c0c1ad270b506082998829fa26e3d3e4c92
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-13 23:50:38 +0200

    Update commande pnastyy

--- cmds/pnaasty.js
+++ cmds/pnaasty.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Profile GLR")
     .setThumbnail(message.guild.iconURL())
     .addField(
-      "Pour aller sur le profil de **Naasty**",
-      "[Clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717464539318190131)"
+      "Pour aller sur le profil de",
+      "<@528668997969903656> [clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/717464539318190131)"
     )
     .setFooter(
       `No Limit - Profil GLR `,
