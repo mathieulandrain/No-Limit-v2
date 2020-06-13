bac30bd7db90cb5a61232865fb97b55547fe69e7
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-13 23:59:16 +0200

    Update commande psquish

--- cmds/psquish.js
+++ cmds/psquish.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Profile GLR")
     .setThumbnail(message.guild.iconURL())
     .addField(
-      "Pour aller sur le profil de **Squish**",
-      "[Clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/719701028492279848)"
+      "Pour aller sur le profil de",
+      "<@240458921326673920> [clic ici](https://discordapp.com/channels/683734629945311349/717462020240441424/719701028492279848)"
     )
     .setFooter(
       `No Limit - Profil GLR `,
