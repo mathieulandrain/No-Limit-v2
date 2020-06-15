b1ae06847b4cb096e9ab299e662ff2b6eafdb1eb
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:17:38 +0200

    Ajout des modules export dans ptrash

--- cmds/Profils/ptrash.js
+++ cmds/Profils/ptrash.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `ptrash`,
+  aliases: ["ptrash"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
