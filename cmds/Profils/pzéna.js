42fec97238051c9c62a3fa8daa8b73d4671497af
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:20:55 +0200

    Ajout des modules export dans pzena

--- cmds/Profils/pzéna.js
+++ cmds/Profils/pzéna.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `pzéna`,
+  aliases: ["pzena", "pdoyenne"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
