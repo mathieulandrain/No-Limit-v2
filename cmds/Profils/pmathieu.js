c18764047529003a6241c069b4127d3e9706efd0
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:10:26 +0200

    Ajout des modules export dans pmathieu

--- cmds/Profils/pmathieu.js
+++ cmds/Profils/pmathieu.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `pmathieu`,
+  aliases: ["pmath", "pzeus"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
