46bca4a6938144216f42600c2d5b9f6f40d89edf
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:14:52 +0200

    Ajout des modules export dans pprodu

--- cmds/Profils/pprodu006.js
+++ cmds/Profils/pprodu006.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `pprodu`,
+  aliases: ["pprodu"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
