781aded4c9d3858db7e3d57960ea0a6e5cb6ca2c
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:16:20 +0200

    Ajout des modules export dans psow

--- cmds/Profils/psowwood.js
+++ cmds/Profils/psowwood.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `psow`,
+  aliases: ["psowwoow"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
