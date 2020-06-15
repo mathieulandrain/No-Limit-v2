e3a664eb0ee9626aa10012caa11d45c39abe067f
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:04:25 +0200

    Ajout des modules export dans pelon

--- cmds/Profils/pelon.js
+++ cmds/Profils/pelon.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `pelon`,
+  aliases: ["pelon"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
