b2062bd423b915315571fd3df8e22cb6094e9b57
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:16:45 +0200

    Ajout des modules export dans psquish

--- cmds/Profils/psquish.js
+++ cmds/Profils/psquish.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `psquish`,
+  aliases: ["psquish"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
