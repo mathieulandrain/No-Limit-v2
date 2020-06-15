1ef4898396ec2a21c7e245efea17da0f2e7fa786
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:19:00 +0200

    Ajout des modules export dans pwal

--- cmds/Profils/pwal.js
+++ cmds/Profils/pwal.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `pwal`,
+  aliases: ["pwalnate"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
