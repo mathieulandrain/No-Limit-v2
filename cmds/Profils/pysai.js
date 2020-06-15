8ee577cb494e9f42146eeead562217807ebb5e57
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:19:58 +0200

    Ajout des modules export dans pysai

--- cmds/Profils/pysai.js
+++ cmds/Profils/pysai.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `pyshai`,
+  aliases: ["pysai"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
