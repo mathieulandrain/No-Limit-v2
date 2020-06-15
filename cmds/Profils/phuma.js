783ae0b79aaba27876b82058b4957957123768b0
Mathieu Landrain <landrainmathieu2@gmail.com>
2020-06-15 04:05:45 +0200

    Ajout des modules export dans phuma

--- cmds/Profils/phuma.js
+++ cmds/Profils/phuma.js
@@ -19,4 +19,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: `phuma`,
+  aliases: ["phumanity"],
+  category: "profils",
+  description: "Avoir accès au profil GLR d'un membre",
+  usage: "",
 };
